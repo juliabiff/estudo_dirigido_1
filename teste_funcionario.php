718efd70d25d949a921a2fb742602f474bce4e11
juliabiff <44979671+juliabiff@users.noreply.github.com>
2018-11-12 16:54:16 -0200

    Create teste_funcionario.php

---
+++ teste_funcionario.php
@@ -0,0 +1,31 @@
+<?php
+
+
+Class Funcionario{
+
+    public $departamento;
+    public $salario;
+    public $dataEntrada;
+    public $CPF;
+
+
+
+/*
+    public function recebeAumento(){
+        $salarioAumentado = $this->salario * 0,1;
+}
+
+
+
+    public function calculaGanhoAnual(){
+        $salarioAnual = $this->salario * 12;
+    }
+
+
+
+
+}
+*/
+empregado recebe novo funcionario
+
+}
