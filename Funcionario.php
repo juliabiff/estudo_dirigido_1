0e8ca97f547abe8dd7c56c3c24b4d3e5df0ef155
juliabiff <44979671+juliabiff@users.noreply.github.com>
2018-11-12 16:44:28 -0200

    Create Funcionario.php

---
+++ Funcionario.php
@@ -0,0 +1,26 @@
+<?php
+
+Class Funcionario{
+
+    public $departamento;
+    public $salario;
+    public $dataEntrada;
+    public $CPF;
+
+    
+
+
+    public function recebeAumento(){
+        $salarioAumentado = $salario * 0,1;
+}
+
+
+
+    public function calculaGanhoAnual(){
+        $salarioAnual = $salario * 12;
+    }
+
+
+
+
+}
