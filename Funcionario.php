240a8e27cfe494724bf0582945e1ab0b0900dd38
juliabiff <44979671+juliabiff@users.noreply.github.com>
2018-11-19 16:59:14 -0200

    Update Funcionario.php

--- Funcionario.php
+++ Funcionario.php
@@ -24,3 +24,54 @@ Class Funcionario{
 
 
 }
+
+
+-----
+    
+    
+    <?php
+/**
+ * Created by PhpStorm.
+ * User: aluno
+ * Date: 12/11/2018
+ * Time: 16:30
+ */
+
+Class Funcionario{
+
+    public $departamento;
+    public $salario;
+    public $dataEntrada;
+    public $CPF;
+
+    public function recebeAumento(){
+        $aumento = $this->salario * 0.1;
+        $this->salario = $this->salario + $aumento;
+    }
+
+    public function calculaGanhoAnual(){
+        $salarioAnual = $this->salario * 12;
+    }
+
+
+    public function mostra(){
+        echo "O funcionario do departamento {$this->departamento} tem o salario de {$this->salario} ... \n";
+    }
+
+}//fim da classe
+
+
+//TESTE
+
+//$clayton = new Funcionario();
+//$clayton->departamento = "Informatica";
+//$clayton->salario = 1000;
+//$clayton->recebeAumento();
+//$clayton->mostra();
+//
+//
+//
+//$crys = new Funcionario();
+//$crys->departamento = "Gerencia";
+//$crys->salario = 9000;
+//print_r($crys);
