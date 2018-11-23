33f870c9f8c72118eabc684e853b3232e8aa5eca
juliabiff <44979671+juliabiff@users.noreply.github.com>
2018-11-22 23:46:31 -0200

    Update Funcionario.php

--- Funcionario.php
+++ Funcionario.php
@@ -37,6 +37,45 @@ Class Funcionario{
  * Time: 16:30
  */
 
+
+
+}//fim da classe
+
+
+//TESTE
+
+//$clayton = new Funcionario();
+//$clayton->departamento = "Informatica";
+//$clayton->salario = 1000;
+//$clayton->recebeAumento();
+//$clayton->mostra();
+//
+//
+//
+//$crys = new Funcionario();
+//$crys->departamento = "Gerencia";
+//$crys->salario = 9000;
+//print_r($crys);
+
+
+
+
+  //Questao 12
+
+public function informarDataEntrada(Data $data){
+
+    if($data->dia > 31 || $data->mes > 12){
+    
+        echo "data incorreta";
+    
+    }else{
+    
+        $this->dataEntrada = $data;
+    
+    }
+
+}
+
 Class Funcionario{
 
     public $departamento;
@@ -57,21 +96,9 @@ Class Funcionario{
     public function mostra(){
         echo "O funcionario do departamento {$this->departamento} tem o salario de {$this->salario} ... \n";
     }
-
-}//fim da classe
-
-
-//TESTE
-
-//$clayton = new Funcionario();
-//$clayton->departamento = "Informatica";
-//$clayton->salario = 1000;
-//$clayton->recebeAumento();
-//$clayton->mostra();
-//
-//
-//
-//$crys = new Funcionario();
-//$crys->departamento = "Gerencia";
-//$crys->salario = 9000;
-//print_r($crys);
+    
+    public function __toString(){
+    
+        return $this->mostra();
+    
+    }
